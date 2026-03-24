2a298a8d5d9a4901a31a80e138cbda92038c3926
Simone <info@simoneicardi.com>
2026-03-24 19:51:27 +0100

    primo invio di formato json

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.type("html")
-        .send(`<h1>Titolo di una pagina HTML!<h1>`)
+    const person = {
+        name: "Ted",
+        lastname: "Lasso"
+    };
+
+    res.json(person);
 })
 
 app.listen(port, () => {
