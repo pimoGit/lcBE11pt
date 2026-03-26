1100e76584197fade2498a3ef3932179045e91f3
Simone <info@simoneicardi.com>
2026-03-26 19:33:25 +0100

    setup rotte di crud risorsa pizze complete

--- app.js
+++ app.js
@@ -11,6 +11,35 @@ app.get('/', (req, res) => {
 
 // rotte di CRUD per la risorsa pizze
 
+// index
+app.get('/pizzas', function (req, res) {
+    res.send('Lista delle pizze');
+});
+
+// show
+app.get('/pizzas/:id', function (req, res) {
+    res.send('Dettagli della pizza ' + req.params.id);
+});
+
+// store
+app.post('/pizzas', function (req, res) {
+    res.send('Creazione nuova pizza');
+});
+
+// update
+app.put('/pizzas/:id', function (req, res) {
+    res.send('Modifica integrale della pizza ' + req.params.id);
+});
+
+// modify
+app.patch('/pizzas/:id', function (req, res) {
+    res.send('Modifica parziale della pizza ' + req.params.id);
+});
+
+// destroy
+app.delete('/pizzas/:id', function (req, res) {
+    res.send('Eliminazione della pizza ' + req.params.id);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
