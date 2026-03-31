a9102bf6a0eeb868be620e1aa25f041285034776
Simone <info@simoneicardi.com>
2026-03-31 19:59:39 +0200

    aggiungi logica destroy

--- routers/pizzas.js
+++ routers/pizzas.js
@@ -72,7 +72,28 @@ router.patch('/:id', function (req, res) {
 
 // destroy
 router.delete('/:id', function (req, res) {
-    res.send('Eliminazione della pizza ' + req.params.id);
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il pizza tramite id
+    const pizza = menuPizze.find(pizza => pizza.id === id);
+
+    // Piccolo controllo
+    if (!pizza) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Pizza non trovata"
+        })
+    }
+
+    // Rimuoviamo la pizza dal menu
+    menuPizze.splice(menuPizze.indexOf(pizza), 1);
+
+    // Restituiamo lo status corretto
+    res.sendStatus(204)
 });
 
 // esporto il router per poterlo usare i app
