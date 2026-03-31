f75f342686718840472c509fdf5852557472b366
Simone <info@simoneicardi.com>
2026-03-31 20:46:51 +0200

    dividi le logiche della risorsa nel controller

---
+++ controllers/pizzaController.js
@@ -0,0 +1,89 @@
+// importo dati del menu pizze
+const menuPizze = require('./../data/menu');
+
+// elenco funzioni relative alle rotte della risorsa pizze
+
+function index(req, res) {
+    //Inizialmente, il menu filtrato corrisponde a quello originale
+    let filteredMenu = menuPizze;
+
+    // Se la richiesta contiene un filtro, allora filtriamo il menu
+    if (req.query.ingredient) {
+        filteredMenu = menuPizze.filter(
+            pizza => pizza.ingredients.includes(req.query.ingredient)
+        );
+    }
+
+    // creo oggetto nuovo per la formattazione completa della risposta
+    const menuPizzeCompleto = {
+        numeroPizze: filteredMenu.length,
+        listaPizze: filteredMenu
+    }
+
+    res.json(menuPizzeCompleto);
+}
+
+function show(req, res) {
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il pizza tramite id
+    const pizzaTrovata = menuPizze.find(pizza => pizza.id === id);
+
+    // Facciamo il controllo
+    if (!pizzaTrovata) {
+
+        //Imposto lo status 404
+        res.status(404)
+
+        // Restituisco un JSON con le altre informazioni
+        return res.json({
+            error: "Not Found",
+            message: "Pizza non trovata"
+        })
+    }
+
+
+    // Restituiamolo sotto forma di JSON   
+    res.json(pizzaTrovata);
+}
+
+function store(req, res) {
+    res.send('Creazione nuova pizza');
+}
+
+function update(req, res) {
+    res.send('Modifica integrale della pizza ' + req.params.id);
+}
+
+function modify(req, res) {
+    res.send('Modifica parziale della pizza ' + req.params.id);
+}
+
+function destroy(req, res) {
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il pizza tramite id
+    const pizza = menuPizze.find(pizza => pizza.id === id);
+
+    // Piccolo controllo
+    if (!pizza) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Pizza non trovata"
+        })
+    }
+
+    // Rimuoviamo la pizza dal menu
+    menuPizze.splice(menuPizze.indexOf(pizza), 1);
+
+    // Restituiamo lo status corretto
+    res.sendStatus(204)
+}
+
+// esportiamo le funzioni per il router
+module.exports = { index, show, store, update, modify, destroy }
--- routers/pizzas.js
+++ routers/pizzas.js
@@ -3,98 +3,27 @@ const express = require('express')
 // estrapolo e uso la parte di router
 const router = express.Router();
 
-// importo dati del menu pizze
-const menuPizze = require('./../data/menu');
+// importo il controller della risorsa pizze
+const pizzaController = require('./../controllers/pizzaController');
 
 // definisco le varie rotte relative alla risorsa specifica
 // index
-router.get('/', function (req, res) {
-
-    //Inizialmente, il menu filtrato corrisponde a quello originale
-    let filteredMenu = menuPizze;
-
-    // Se la richiesta contiene un filtro, allora filtriamo il menu
-    if (req.query.ingredient) {
-        filteredMenu = menuPizze.filter(
-            pizza => pizza.ingredients.includes(req.query.ingredient)
-        );
-    }
-
-    // creo oggetto nuovo per la formattazione completa della risposta
-    const menuPizzeCompleto = {
-        numeroPizze: filteredMenu.length,
-        listaPizze: filteredMenu
-    }
-
-    res.json(menuPizzeCompleto);
-});
+router.get('/', pizzaController.index);
 
 // show
-router.get('/:id', function (req, res) {
-    // recuperiamo l'id dall' URL e trasformiamolo in numero
-    const id = parseInt(req.params.id)
-
-    // cerchiamo il pizza tramite id
-    const pizzaTrovata = menuPizze.find(pizza => pizza.id === id);
-
-    // Facciamo il controllo
-    if (!pizzaTrovata) {
-
-        //Imposto lo status 404
-        res.status(404)
-
-        // Restituisco un JSON con le altre informazioni
-        return res.json({
-            error: "Not Found",
-            message: "Pizza non trovata"
-        })
-    }
-
-
-    // Restituiamolo sotto forma di JSON   
-    res.json(pizzaTrovata);
-});
+router.get('/:id', pizzaController.show);
 
 // store
-router.post('/', function (req, res) {
-    res.send('Creazione nuova pizza');
-});
+router.post('/', pizzaController.store);
 
 // update
-router.put('/:id', function (req, res) {
-    res.send('Modifica integrale della pizza ' + req.params.id);
-});
+router.put('/:id', pizzaController.update);
 
 // modify
-router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale della pizza ' + req.params.id);
-});
+router.patch('/:id', pizzaController.modify);
 
 // destroy
-router.delete('/:id', function (req, res) {
-    // recuperiamo l'id dall' URL e trasformiamolo in numero
-    const id = parseInt(req.params.id)
-
-    // cerchiamo il pizza tramite id
-    const pizza = menuPizze.find(pizza => pizza.id === id);
-
-    // Piccolo controllo
-    if (!pizza) {
-        res.status(404);
-
-        return res.json({
-            status: 404,
-            error: "Not Found",
-            message: "Pizza non trovata"
-        })
-    }
-
-    // Rimuoviamo la pizza dal menu
-    menuPizze.splice(menuPizze.indexOf(pizza), 1);
-
-    // Restituiamo lo status corretto
-    res.sendStatus(204)
-});
+router.delete('/:id', pizzaController.destroy);
 
 // esporto il router per poterlo usare i app
 module.exports = router;
