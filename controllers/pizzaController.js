c3689f7a241011317bef80795042bbf32d00f365
Simone <info@simoneicardi.com>
2026-04-23 19:30:42 +0200

    logica sql per show

--- controllers/pizzaController.js
+++ controllers/pizzaController.js
@@ -19,30 +19,16 @@ function index(req, res) {
 
 function show(req, res) {
     // recuperiamo l'id dall' URL e trasformiamolo in numero
-    const id = parseInt(req.params.id)
+    const id = req.params.id
 
-    // errore a caso
-    // ciao();
+    // query da eseguire con ?seganposto per prepared statement
+    const sql = 'SELECT * FROM pizzas WHERE id = ?';
 
-    // cerchiamo il pizza tramite id
-    const pizzaTrovata = menuPizze.find(pizza => pizza.id === id);
-
-    // Facciamo il controllo
-    if (!pizzaTrovata) {
-
-        //Imposto lo status 404
-        res.status(404)
-
-        // Restituisco un JSON con le altre informazioni
-        return res.json({
-            error: "Not Found",
-            message: "Pizza non trovata"
-        })
-    }
-
-
-    // Restituiamolo sotto forma di JSON   
-    res.json(pizzaTrovata);
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Pizza not found' });
+        res.json(results[0]);
+    });
 }
 
 function store(req, res) {
